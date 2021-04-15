173e9c73bee6b2d7de8998d2ed52d79644154274
Lord-Uji <77410944+Lord-Uji@users.noreply.github.com>
2021-04-15 11:07:54 -0500

    Sticky Nav & Media Queries

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function(){
 
+  // Search Bar Code
 /*
     //Call jQuery before below code
 $('.main-btn').click(function() {
@@ -48,7 +49,78 @@ $('.main-btn').click(function() {
     }
   };
 */
-  
+
+
+/* For sticky navbar*/
+$('.js--section-search').waypoint(function(direction){
+ 
+  if (direction == "down"){
+
+      $('nav').addClass('sticky');
+
+  } else {
+
+     $('nav').removeClass('sticky');
+ }
+     
+ }, {
+     offset: '100px;'
+});
+
+
+
+
+
+/*-- Mobile navigation--*/
+$('.js--nav-icon, .js--main-nav a, .logo').click(function(element){
+ var nav = $('.js--main-nav');
+ var icon = $('.js--nav-icon i');
+
+ //Gets the class name of the element that triggered the event
+ var clicked = element.target.className;
+
+ //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
+ if (icon.hasClass('ion-navicon-round') && clicked == 'logo')
+     return;
+
+ //Opens and closes the menu
+ if ($(window).width() < 767){
+     nav.slideToggle(200);
+ }
+
+ //Changes icon states of the menu button
+ if (icon.hasClass('ion-navicon-round')) {
+     icon.addClass('ion-close-round');
+     icon.removeClass('ion-navicon-round');
+ } else {
+     icon.addClass('ion-navicon-round');
+     icon.removeClass('ion-close-round');
+ }
+});
+
+
+$(window).resize(function(){
+ var nav = $('.js--main-nav');
+ var icon = $('.js--nav-icon i');
+
+ if ($(window).width() > 750){
+     nav.css("display", "block");
+     icon.addClass('ion-close-round');
+     icon.removeClass('ion-navicon-round');
+ } else {
+     nav.css("display", "none");
+     icon.addClass('ion-navicon-round');
+     icon.removeClass('ion-close-round');
+ }
+
+});
+
+
+
+
+
+/*__________________________________________________________________*/  
+/* For SWIPER functionality */
 
 const swiper1 = new Swiper('.swiper1', {
 
